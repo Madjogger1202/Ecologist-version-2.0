0e622d524de2e989d0dda28a14e80bc2945f7cce
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2021-03-13 13:20:16 +0300

    start version

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,38 @@
 
 #define GP2Y_PIN A0
 
+struct frstSrt
+{
+    uint8_t id = 0;   // 1 byte
+    int16_t x_acs;    // 2 bytes
+    int16_t y_acs;    // 2 bytes
+    int16_t z_acs;    // 2 bytes
+    float presssure;  // 4 bytes
+    uint32_t counter; // 4 bytes
+}fastData;            // 15 bytes: 15 / 250Kbps = 0.4 ms - min max delay 
 
+struct secStr
+{
+    uint8_t id = 1;   // 1 byte
+    uint16_t pm25;    // 2 bytes
+    uint16_t tVOC;    // 2 bytes
+    uint16_t rad_qw;  // 2 bytes
+    uint16_t co2_ppm; // 2 bytes
+    float lanGPS;     // 4 bytes
+    float lonGPS;     // 4 bytes
+    float humidVal;   // 4 bytes
+    float temp;       // 4 bytes
+    uint32_t counter; // 4 bytes
+} slowData;           // 29 bytes: 29 / 250Kbps = 0.9ms - max max delay
 
-
-
-
+bool measureAcs(int16_t x, int16_t y, int16_t z);
+uint16_t measureRadiation();
+float measureTemp();
+bool getGPSdata(float LAN, float LON);
+float measurePressure();
+uint16_t measureTVOC();
+uint16_t getRadVal();
+float getHumid(); 
 
 void setup()
 {
@@ -28,3 +55,60 @@ void loop()
 
   
 }
+
+
+
+bool measureAcs(int16_t x, int16_t y, int16_t z)
+{
+
+    return 0;
+}
+
+uint16_t measureRadiation()
+{
+
+    
+}
+
+
+float measureTemp()
+{
+
+    
+}
+
+
+bool getGPSdata(float LAN, float LON)
+{
+
+    return 0;
+}
+
+
+float measurePressure()
+{
+
+    
+}
+
+
+uint16_t measureTVOC()
+{
+
+    
+}
+
+
+uint16_t getRadVal()
+{
+
+    
+}
+
+
+float getHumid()
+{
+
+    
+}
+ 
